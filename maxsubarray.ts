37e1744d40b311a437aa225176fcca4fe69a75fc
ozyirl <133862552+ozyirl@users.noreply.github.com>
2025-11-04 11:07:02 -0700

    queu

--- maxsubarray.ts
+++ maxsubarray.ts
@@ -1,5 +1,7 @@
 const array = [-2, 1, -3, 4, -1, 2, 1, -5, 4];
 
+const array2 = [-5, 2, 3, -1, 7, -6, 4];
+
 function maxSubArray(nums: number[]): number {
   let c = nums[0];
   let b = nums[0];
@@ -12,3 +14,4 @@ function maxSubArray(nums: number[]): number {
 }
 
 console.log(maxSubArray(array));
+console.log(maxSubArray(array2));
