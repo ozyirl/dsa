6572ebb0dd4d9c2a724ccbe2af22fa25df818b61
ozyirl <133862552+ozyirl@users.noreply.github.com>
2025-11-18 10:57:20 -0700

    jump game 2

---
+++ jumpcount.ts
@@ -0,0 +1,27 @@
+function jump(nums: number[]): number {
+  let jumps = 0;
+  let currentEnd = 0;
+  let farthest = 0;
+
+  for (let i = 0; i < nums.length - 1; i++) {
+
+        farthest = Math.max(farthest,i + nums[i])
+     
+   
+    if(i === currentEnd){
+    
+
+        jumps++
+        currentEnd = farthest
+    }
+
+
+  }
+
+  return jumps;
+}
+
+
+const nums = [2, 3, 1, 1, 4];
+
+console.log(jump(nums));
